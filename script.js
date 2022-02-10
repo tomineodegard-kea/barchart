8cf15991337257aac9202c38ea4a57b824044f38
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-10 12:46:20 +0100

    added css and styling to show the bars

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+// 10.02.22 - Bar chart, using MVC pattern
 "use strict";
 
 window.addEventListener("load", init);
 
-const model = [8, 12, 32, 4, 13, 24, 23, 22, 1, 0, 30, 7, 3, 6, 3, 19, 24, 29, 14, 16, 3, 1, 9, 8, 30, 12, 0, 30, 7, 3, 6, 3, 19, 29, 15, 2, 8]
-function init() [
-    console.log("init")
-]
+const model = [8, 12, 32, 4, 13, 24, 23, 22, 1, 0, 30, 7, 3, 6, 3, 19, 24, 29, 14, 16, 3, 1, 9, 8, 30, 12, 0, 30, 7, 3, 6, 3, 19, 29, 15, 2, 8];
+const bar = document.querySelector("#bar");
+
+function init() {
+  console.log("init");
+}
 
 function loop() {}
 
+// model
 function getNumberOfCustomers() {}
 
-function displayData(){}
+// view
+function displayData() {}
 
+// controller
 function modifyModel() {}
