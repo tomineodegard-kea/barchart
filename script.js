c9e1e6b0a6624abba8ee5d574b550b39ddae00a8
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-10 12:31:10 +0100

    setting up the structure

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+window.addEventListener("load", init);
+
+const model = [8, 12, 32, 4, 13, 24, 23, 22, 1, 0, 30, 7, 3, 6, 3, 19, 24, 29, 14, 16, 3, 1, 9, 8, 30, 12, 0, 30, 7, 3, 6, 3, 19, 29, 15, 2, 8]
+function init() [
+    console.log("init")
+]
+
+function loop() {}
+
+function getNumberOfCustomers() {}
+
+function displayData(){}
+
+function modifyModel() {}
